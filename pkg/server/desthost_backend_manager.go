afe5f83d652cc283a96faadcc644ece6b6335b5e
Bryan Cox <brcox@redhat.com>
2026-05-18 09:50:34 -0400

    UPSTREAM: <carry>: load balance traffic across redundant backends

--- pkg/server/desthost_backend_manager.go
+++ pkg/server/desthost_backend_manager.go
@@ -79,25 +79,10 @@ func (dibm *DestHostBackendManager) Backend(ctx context.Context) (*Backend, erro
 	if destHost != "" {
 		bes, exist := dibm.backends[destHost]
 		if exist && len(bes) > 0 {
-			var firstDrainingBackend *Backend
-
-			// Find a non-draining backend for this destination host
-			for _, backend := range bes {
-				if !backend.IsDraining() {
-					klog.V(5).InfoS("Get the backend through the DestHostBackendManager", "destHost", destHost)
-					return backend, nil
-				}
-				// Keep track of first draining backend as fallback
-				if firstDrainingBackend == nil {
-					firstDrainingBackend = backend
-				}
-			}
-
-			// All backends for this destination are draining, use one as fallback
-			if firstDrainingBackend != nil {
-				klog.V(3).InfoS("All backends for destination host are draining, using one as fallback", "destHost", destHost)
-				return firstDrainingBackend, nil
-			}
+			klog.V(5).InfoS("Get the backend through the DestHostBackendManager", "destHost", destHost)
+			//TODO: change dibm.random.Intn(len(bes)) to be in sync with community when they fix
+			// https://github.com/kubernetes-sigs/apiserver-network-proxy/issues/261
+			return dibm.backends[destHost][dibm.random.Intn(len(bes))], nil
 		}
 	}
 	return nil, &ErrNotFound{}
